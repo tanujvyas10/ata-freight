57b6bbe1848ca7c1436763f5892315dbb59e7806
tanuj <tanujvyas10@gmail.com>
2020-05-17 00:04:52 +0530

    db commit

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,4 @@
 module.exports = {
-  mongoURI: 'mongodb://localhost/ata-freights1',
+  mongoURI: 'mongodb+srv://tan10vyaw:WO98K8zX4Orx5vYk@cluster0-yysms.mongodb.net/test?retryWrites=true&w=majority',
   secretOrKey: 'secret'
 };
